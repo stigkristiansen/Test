32cb8ac63c55f1c10649554829c7eded6cae909e
stigkristiansen <stigkristiansen@outlook.com>
2017-01-04 20:20:58 +0100

    .

--- Child/module.php
+++ Child/module.php
@@ -7,7 +7,7 @@ class TestChild extends IPSModule
     
     public function Create() {
         parent::Create();
-        $this->ConnectParent("{4EF96162-A853-44FC-86EF-07B85A1F4A36}");
+        $this->ConnectParent("{42DF61AE-0745-47D5-B12C-63819FA5DED7}");
 		
     }
 
